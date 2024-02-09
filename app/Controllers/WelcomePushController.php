0800e7817a6a815c905465317544baba059da094
bobih <bobby.khrisna@gmail.com>
2024-02-09 15:32:14 +0700

    update

--- app/Controllers/WelcomePushController.php
+++ app/Controllers/WelcomePushController.php
@@ -1,42 +1,59 @@
-<?php
-
-namespace App\Controllers;
-
-use App\Controllers\BaseController;
-use App\Models\UserModel;
-
-
-class WelcomePushController extends BaseController
-{
-
-    public function sendWelcome(){
-
-        echo "OK";
-
-
-       
-        $userid = 66;
-        $userModel = new UserModel();
-        $user = $userModel->find($userid);
-
-        
-        
-        $fcmtoken = $user['fcmtoken'];
-
-       $fcmController = new FcmController();
-        $title = "";
-        $message = "";
-        $payload = "";
-       
-        echo "<pre>";
-        print_r($user);
-        echo "</pre>";
-        echo $fcmtoken . "-" . $user['nama'];
-      //$pushWelcome = $fcmController->sendWelcomeNotification($fcmtoken,$title,$payload,$message);
-
-       
-
-    }
-
-
+<?php
+
+
+
+namespace App\Controllers;
+
+
+
+use App\Controllers\BaseController;
+
+use App\Models\UserModel;
+
+
+
+
+
+class WelcomePushController extends BaseController
+{
+
+    public function sendWelcome(){
+        echo "OK";
+        $userid = 66;
+        $userModel = new UserModel();
+        $user = $userModel->find($userid);
+        $fcmtoken = $user['fcmtoken'];
+
+       $fcmController = new FcmController();
+
+        $title = "";
+
+        $message = "";
+
+        $payload = "";
+
+       
+
+        echo "<pre>";
+
+        print_r($user);
+
+        echo "</pre>";
+
+        echo $fcmtoken . "-" . $user['nama'];
+
+      //$pushWelcome = $fcmController->sendWelcomeNotification($fcmtoken,$title,$payload,$message);
+
+
+
+       
+
+
+
+    }
+
+
+
+
+
 }
